f59563589e6d834a9c64eda817df651edac03e64
orn-chfotm <chfotm@gmail.com>
2025-08-22 01:18:02 +0900

    2025-08-22 3강 타입스크립트 타입 / 호환성

---
+++ section3/src/chapter2.ts
@@ -0,0 +1,64 @@
+/**
+ * Unknown 타입
+ */
+
+function unknownExam() {
+  let a: unknown = 1;
+  let b: unknown = "hello";
+  let c: unknown = true;
+  let d: unknown = null;
+  let e: unknown = undefined;
+
+  let unknownVal: unknown;
+
+  // let num: number = unknownVal;
+  // let str: string = unknownVal;
+  // let bool: boolean = unknownVal;
+}
+
+/**
+ * Never 타입
+ */
+
+function neverExam() {
+  function neverFunc(): never {
+    while (true) {}
+  }
+
+  let num: number = neverFunc();
+  let srt: string = neverFunc();
+  let bool: boolean = neverFunc();
+
+  // let never1: never = 10;
+  // let never2: never = "string";
+  // let never3: never = true;
+}
+
+/**
+ * Void 타입
+ */
+
+function voidExam() {
+  function voidFunc(): void {
+    console.log("hi");
+    return undefined;
+  }
+
+  let voidVar: void = undefined;
+}
+
+/**
+ * any 타입
+ */
+
+function anyExam() {
+  let unknownVar: unknown;
+  let anyVar: any;
+  let undefinedVar: undefined;
+  let neverVar: never;
+
+  anyVar = unknownVar;
+  undefinedVar = anyVar;
+
+  // neverVar = anyVar;
+}
