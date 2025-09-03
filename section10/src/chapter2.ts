bde04a2ff40650d309cba34f89dd72721d770374
orn-chfotm <chfotm@gmail.com>
2025-09-04 06:17:56 +0900

    2025-09-04 10강 유틸리티 완강

--- section10/src/chapter2.ts
+++ section10/src/chapter2.ts
@@ -28,9 +28,46 @@ const legacyPost: Pick<Post, "title" | "content"> = {
  * -> 생략하다, 빼다
  * -> 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
  */
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// T = Post, K = 'title'
+// Pick<Post, Exclude<keyof Post, 'title>>
+// Pick<Post, Exclude<'title' | 'content' | 'tags' | 'thumbnailURL', 'title'>>
+// Pick<Post, 'content' | 'tags' | 'thumbnailURL'>
+// 'content' | 'tags' | 'thumbnailURL'
 
 const noTitlePost: Omit<Post, "title"> = {
   content: "",
   tags: [],
   thumbnailURL: "",
 };
+
+/**
+ * Record<K, V>
+ *
+ * -> 객체 타입을 새롭게 정의할때 인덱스 시그니처처럼 유연하지만 조금은 제약이 있는
+ * -> K 는 내부 객체 키의 구분, V 는 공통으로 들어갈 프로퍼티
+ */
+
+// type ThumbnailLegacy = {
+//   large: {
+//     url: string;
+//   };
+//   medium: {
+//     url: string;
+//   };
+//   small: {
+//     url: string;
+//   };
+//   watch: {
+//     url: string;
+//   };
+// };
+
+type Record<K extends keyof any, V> = {
+  [key in K] : V
+}
+
+type Thumbnail = Record<
+  "large" | "medium" | "small" | "watch",
+  { url: string; size: number }
+>;
